c06a3776fa603c623d880108c192a10859470dca
Archishman-Naskar <archishmannskr2005@gmail.com>
2025-10-27 05:04:07 +0530

    Adding middlewares and routes

---
+++ middleware.ts
@@ -0,0 +1,47 @@
+import NextAuth from "next-auth";
+
+import {
+  DEFAULT_LOGIN_REDIRECT,
+  apiAuthPrefix,
+  publicRoutes,
+  authRoutes,
+} from "@/routes";
+import authConfig from "./auth.config";
+
+const { auth } = NextAuth(authConfig);
+
+export default auth((req) => {
+  const { nextUrl } = req;
+  const isLoggedIn = !!req.auth;
+
+  const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+
+  const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+
+  const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+  if (isApiAuthRoute) {
+    return null;
+  }
+
+  if (isAuthRoute) {
+    if (isLoggedIn) {
+      return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+    }
+    return null;
+  }
+
+  if (!isLoggedIn && !isPublicRoute) {
+    return Response.redirect(new URL("/auth/sign-in", nextUrl));
+  }
+
+  //Response.redirect() is used to redirect the user’s browser to another URL in Next.js middleware, based on a specific condition.
+  // Response.redirect(new URL("/auth/sign-in", nextUrl) here user will be redirected to /auth/sign-in intesd of original nextUrl it choosed
+  return null;
+  //but if null is returnd then user will contunue to destination he choosed
+});
+
+export const config = {
+  // copied from clerk
+  matcher: ["/((?!.+\\.[\\w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
+};
